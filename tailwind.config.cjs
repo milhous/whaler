5ca8ecb9e755eaa4c52744d115d616f22751e6a0
Milhous <milhous@sina.com>
2023-03-20 09:20:11 +0800

    A github

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: "class",
-    content: ["./app/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        colors: {
-            inherit: "inherit",
-            transparent: "transparent",
-            currentColor: "currentColor",
-            white: "#fff",
-            black: "#000",
-            yellow: "#f4a249",
-            gray: "#dce0e4",
-            "gray-dark": "#333",
-        },
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      inherit: "inherit",
+      transparent: "transparent",
+      currentColor: "currentColor",
+      white: "#fff",
+      black: "#000",
+      orange: "#f4a249",
+      gray: "#dce0e4",
+      "gray-dark": "#333",
     },
-    plugins: [],
+  },
+  plugins: [],
 };
